c7aa41229d3501139e9cdfe3a54da8159e4d4330
Anders Emil Nielsen <aemilnielsen@gmail.com>
2015-03-05 15:18:40 +0100

    TMP

--- routes/documents.js
+++ routes/documents.js
@@ -20,15 +20,29 @@ module.exports = function() {
 
   router.post('/docs', isLoggedIn, function(req, res) {
     var currentUser = req.user;
-    console.log(req.body);
-    res.send("ok");
-    // Document.create({
-    //   creator_id: currentUser.id,
-    //   permitted_user_ids: [currentUser.id],
-    //   doc_id: generateUUID()
-    // }, function(error, document) {
-    //   if (error) throw error;
-    // });
+    var name = req.body.name || 'Undefined';
+    Document.create({
+      creator_id: currentUser.id,
+      name: name,
+      permitted_user_ids: [currentUser.id],
+      doc_id: generateUUID()
+    }, function(error, document) {
+      if (error) throw error;
+      res.send(JSON.stringify(document));
+    });
+  });
+
+  router.put('/docs/:doc_id/permitted_users/:permitted_user_id', isLoggedIn, function(req, res) {
+    console.log(req.params);
+    var doc_id = req.params('doc_id');
+    Document.docPermittedFor(doc_id, req.user.id, function(error, document) {
+      if (error) throw error;
+      if (document) {
+        Document.addPermissionTo(doc_id, req.params('permitted_user_id'), function(
+      } else {
+        res.send(403);
+      }
+    });
   });
 
   return router;
