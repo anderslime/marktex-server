8db6aef0230d58c0a6a312b418046d61d3a0214e
Andreas Toftegaard <toftegaard.andreas@gmail.com>
2015-03-18 14:39:27 +0100

    only document owner can edit document meta and sharing options

--- models/document.js
+++ models/document.js
@@ -23,15 +23,16 @@ var documentSchema = mongoose.Schema({
   permittedUsers: { type: [userSchema], required: false }
 });
 
-var permittedQuery = function(user_id){ return { '$or': [{ creatorId: user_id }, { 'permittedUsers._id': new ObjectId(user_id) }]}; };
+var readPermittedQuery = function(user_id){ return { '$or': [{ creatorId: user_id }, { 'permittedUsers._id': new ObjectId(user_id) }]}; };
+var readWritePermittedQuery = function(user_id){ return { creatorId: user_id }; };
 
 documentSchema.statics.findPermittedDocsForUser = function(user_id, cb) {
-  this.find(permittedQuery(user_id)).sort('-createdAt').exec(cb);
+  this.find(readPermittedQuery(user_id)).sort('-createdAt').exec(cb);
 };
 
 documentSchema.statics.findPermittedDocForUser = function(doc_id, user_id, cb) {
   if (isValidObjectId(doc_id)) {
-    this.findOne(permittedQuery(user_id), cb);
+    this.findOne(readWritePermittedQuery(user_id), cb);
   } else {
     cb(null, null);
   }
@@ -39,7 +40,7 @@ documentSchema.statics.findPermittedDocForUser = function(doc_id, user_id, cb) {
 
 documentSchema.statics.removeIfPermitted = function(docId, user_id, cb) {
   if (isValidObjectId(docId)) {
-    this.remove(permittedQuery(user_id), cb);
+    this.remove(readWritePermittedQuery(user_id), cb);
   } else {
     cb(null, null);
   }
@@ -48,7 +49,7 @@ documentSchema.statics.removeIfPermitted = function(docId, user_id, cb) {
 documentSchema.statics.updateIfPermitted = function(docId, user_id, data, cb) {
   if (isValidObjectId(docId)) {
     this.findOneAndUpdate(
-      { '$and': [{_id: new ObjectId(docId)}, permittedQuery(user_id)] },
+      { '$and': [{_id: new ObjectId(docId)}, readWritePermittedQuery(user_id)] },
       {'$set':
         {
           name: data.name,
